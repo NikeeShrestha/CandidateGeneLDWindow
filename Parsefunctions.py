b7c0c9c591cff4b7cf3482974abc92fa8996d9b8
NikeeShrestha <nshrest@okstate.edu>
2025-02-26 11:47:03 -0600

    code for both maize and sorghum

---
+++ Parsefunctions.py
@@ -0,0 +1,132 @@
+import pandas as pd
+
+def defparse(astr):
+    dd = {}
+    y = astr.strip().split(';')
+    for a in y:
+        b = a.split('=')
+        dd[b[0]] = b[1]
+    return dd
+
+def annotationparse(afile):
+    annotationfile={}
+    with open(str(afile), 'r') as myfile:
+        for line in myfile:
+            myline=line.strip()
+            if myline.startswith('#'): continue
+            
+            y=myline.split('\t')
+            if len(y)<9: continue
+            
+            gene=y[1]
+            
+            # print(y)
+            if len(y)>10 and y[10]:
+                arabhit=y[10]
+            else: arabhit=''
+                
+            if len(y)>12 and y[12]:
+                arabdefine=y[12]
+            else: arabdefine=''
+            # if len(y)<2: continue
+            
+            if len(y)>13 and y[13]:
+                ricehit=y[13]
+            else: ricehit=''
+            if  len(y)>15 and y[15]:
+                ricedefine=y[15]
+            else: ricedefine=''
+                
+            
+            annotationfile[gene]={'Tairhit':arabhit, 'Tairdefine':arabdefine,
+                                 'Ricehit':ricehit, 'Ricedefine':ricedefine}
+    return annotationfile
+
+def gff3parse(afile, species, intervalstart, intervalend, chrom, snpposition, annotation={}):
+    
+    chrom=chrom
+    intervalstart=int(intervalstart)
+    intervalend=int(intervalend)
+    snpposiiton=int(snpposition)
+    genes={}
+    # count=0
+    with open(str(afile), 'r') as mygff3:
+        for line in mygff3:
+            myline=line.strip()
+            if myline.startswith('#'): continue
+            
+            y=myline.split('\t')
+            
+#             print(y)
+#             count+=1
+            
+#             if count==50: break
+            # print(y[2])
+            if 'scaf' in y[0]: continue
+            if y[2]!='gene': continue
+            
+            
+            chromosome=int(y[0].lower().replace('chr',''))
+            # print(chromosome)
+            
+            if chrom!=chromosome : continue
+            
+            defdict=defparse(y[-1])
+            if species.lower()=='maize':
+                gene=defdict['ID']
+            elif species.lower()=='sorghum':
+                gene=defdict['Name']
+            
+            genestart=int(y[3])
+            geneend=int(y[4])
+            midpoint=(genestart+geneend)/2
+            
+            
+            if (intervalstart <= geneend and genestart <= intervalend):
+                # print(gene)
+                distancemin=int(abs(midpoint-snpposiiton))
+                genes[gene]={'chrom': chromosome, 'start':genestart, 'end':geneend, 'distance_from_snp': distancemin}
+                
+                if annotation and gene in annotation:
+                    function=annotation[gene]
+                    genes[gene].update(function)
+            else:
+                continue
+                
+    return genes
+
+def LDfile(afile, chromcolumn, windowstartcolumn, windowendcolumn, snpcolumn, k, species, pathtogff3,annotationfile=None):
+    try:
+        df=pd.read_excel(str(afile))
+    except Exception:
+        df=pd.read_csv(str(afile))
+
+    # df=pd.read_excel(str(afile))
+    # print(df.shape)
+    
+    if k < 0 or k > df.shape[0]-1: return
+    
+    row=df.iloc[k]
+    # print(row[int(chromcolumn)])
+    chrom=int(str(row[int(chromcolumn)]).lower().replace('chr',''))
+    
+    intervalstart=int(row[int(windowstartcolumn)])
+    
+    intervalend=int(row[int(windowendcolumn)])
+    snp=row[int(snpcolumn)]
+    
+    snpposition=int(str(row[int(snpcolumn)]).split('_')[1])
+    
+    genes=gff3parse(afile=pathtogff3, species=species, intervalstart=intervalstart,
+                    intervalend=intervalend, chrom=chrom, snpposition=snpposition,
+                    annotation=annotationfile)
+    
+    genesdf= pd.DataFrame.from_dict(genes, orient='index')
+    # print(genesdf.head())
+
+    genesdf.to_csv(f'{snp}_candidategenes.csv', index_label='genes')
+    
+    # return genes,genesdf
+    
+    # print(k+1)
+    LDfile(afile,chromcolumn, windowstartcolumn, windowendcolumn, snpcolumn, k+1, species, pathtogff3, annotationfile)
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# CandidateGeneLDWindow
+# CandidateGene within Window
 
 ## Clone this repository
 
@@ -6,10 +6,8 @@ git clone git@github.com:NikeeShrestha/CandidateGeneLDWindow.git
 
 ## Run this code by providing files (see example in the Data folder) to get candidate genes between the window provided for each SNP (see file: Data/LDwindowSignals.xlsx).
 
-## Command for help
+Modify paths to relavent file and species in CandidateGene.py script
 
-python CandidateGene.py or python CandidateGene.py -h
+## Run this in command line
 
-## Command example using files in the Data folder
-
-python CandidateGene.py --gff3 Data/Sbicolor_730_v5.1.gene_exons.gff3 --LDWindow Data/LDwindowSignals.xlsx --annotationfile Data/Sbicolor_454_v3.1.1.annotation_info.txt --abundance Data/abundance.tsv
+python CandidateGene.py
